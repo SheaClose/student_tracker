49fa2fbb64f08e8d63741e9694d22c176561dc1b
SheaClose <SheaClose@gmail.com>
2017-12-22 15:13:39 -0600

    removed repos from db, and now extracting them directly from github

--- db/init/initTables.sql
+++ db/init/initTables.sql
@@ -4,14 +4,3 @@ CREATE TABLE IF NOT EXISTS users (
      first_name varchar(255),
      last_name varchar(255)
 );
-
-CREATE TABLE IF NOT EXISTS repos (
-     id serial primary key,
-     name varchar(255),
-      html_url varchar(255),
-      url varchar(255),
-      created_at varchar(255),
-      updated_at varchar(255),
-      pushed_at varchar(255),
-      language character varying(255)
-);
--- server/api/repos/reposCtrl.js
+++ server/api/repos/reposCtrl.js
@@ -1,9 +1,33 @@
+const axios = require('axios');
+const { github_token } = require('../../../configs/dev.config');
+
 module.exports = {
   getRepos(req, res) {
-    req.app
-      .get('db')
-      .run('select * from repos')
-      .then(resp => res.status(200).json(resp))
-      .catch(err => res.status(500).json(err));
+    let repo_count;
+    axios
+      .get(
+        `https://api.github.com/orgs/DevMountain?access_token=${github_token}`
+      )
+      .then(resp => {
+        repo_count = resp.data.public_repos;
+        const promises = [];
+        for (let i = 1; i <= Math.ceil(repo_count / 100); i++) {
+          promises.push(
+            axios.get(
+              `https://api.github.com/orgs/DevMountain/repos?page=${i}&per_page=100&access_token=${github_token}`
+              // , { headers: { Authorization: `token ${github_token}` } }
+            )
+          );
+        }
+        axios
+          .all(promises)
+          .then(response =>
+            res
+              .status(200)
+              .json(
+                response.reduce((acc, resObj) => [...acc, ...resObj.data], [])
+              )
+          );
+      });
   }
 };
