ac3c23db7a70e42c7fc6d63745f94e6f108af0cf
SheaClose <SheaClose@gmail.com>
2017-12-26 20:24:35 -0600

    removed unnecessary CDM from projects comp

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import RepoDropDown from '../Utils/RepoDropDown';
 
 class Projects extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  componentDidMount() {
-    console.log('componentDidMount');
-  }
-
   render() {
     return (
       <div className="">
