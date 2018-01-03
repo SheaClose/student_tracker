69a9c1788df767a752f4f0d01d9a2c9d696e65da
SheaClose <SheaClose@gmail.com>
2018-01-03 10:13:26 -0600

    cleaned up some unnecessary code.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import CircularProgress from 'material-ui/CircularProgress';
 
 import NavBar from './components/NavBar/NavBar';
 
@@ -12,9 +11,9 @@ import './App.css';
 
 class App extends Component {
   componentWillReceiveProps(newProps) {
-     if (newProps.isAuthed !== this.props.isAuthed && !newProps.isAuthed) {
-       window.location.href = `${rootPath}/auth/devmtn`;
-     }
+    if (newProps.isAuthed !== this.props.isAuthed && !newProps.isAuthed) {
+      window.location.href = `${rootPath}/auth/devmtn`;
+    }
   }
 
   shouldComponentUpdate(nextProps) {
@@ -25,16 +24,14 @@ class App extends Component {
     return (
       <div>
         <NavBar />
-        {this.props.pendingAuth && <CircularProgress size={80} thickness={5} />}
-        {this.props.isAuthed && routes}
+        {routes}
       </div>
     );
   }
 }
 
 App.propTypes = {
-  isAuthed: PropTypes.bool,
-  pendingAuth: PropTypes.bool
+  isAuthed: PropTypes.bool
 };
 
 export default withRouter(
