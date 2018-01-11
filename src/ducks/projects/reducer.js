842f0789c8b34980695c92eccfe202d41ace46a6
Peter Miles <hello@petermiles.io>
2018-01-11 11:08:31 -0600

    fix typos

--- src/ducks/projects/reducer.js
+++ src/ducks/projects/reducer.js
@@ -1,11 +1,16 @@
-import { GET_PROJECTS, ADD_PROJECT, UPDATE_PROJECT, REMOVE_PROJECT } from './actions';
+import {
+  GET_PROJECTS,
+  ADD_PROJECT,
+  UPDATE_PROJECT,
+  REMOVE_PROJECT
+} from './actions';
 
 const initialState = {
   loading: true,
   projects: []
 };
 
-export function projectsReducer(state = initialState, action) {
+export default function projectsReducer(state = initialState, action) {
   switch (action.type) {
   case `${GET_PROJECTS}PENDING`:
     return Object.assign({}, state, {
--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,13 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { projectsReducer } from './projects/reducer';
-import reducer from './reducer';
 import projectsReducer from './projects/reducer';
+import reducer from './reducer';
 
 const reducers = combineReducers({ mainReducer: reducer, projectsReducer });
 
 export default createStore(
-	reducers,
-	composeWithDevTools(applyMiddleware(promiseMiddleware()))
+  reducers,
+  composeWithDevTools(applyMiddleware(promiseMiddleware()))
 );
