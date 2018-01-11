521b39a2a49a43f619aa35752704802363687b42
Peter Miles <hello@petermiles.io>
2018-01-11 11:07:21 -0600

    pushed profile reducers

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -3,8 +3,11 @@ import promiseMiddleware from 'redux-promise-middleware';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { projectsReducer } from './projects/reducer';
 import reducer from './reducer';
-import projectReducer from './projects/reducer';
+import projectsReducer from './projects/reducer';
 
-const reducers = combineReducers({ mainReducer: reducer, projectReducer });
+const reducers = combineReducers({ mainReducer: reducer, projectsReducer });
 
-export default createStore(reducers, composeWithDevTools(applyMiddleware(promiseMiddleware())));
+export default createStore(
+	reducers,
+	composeWithDevTools(applyMiddleware(promiseMiddleware()))
+);
