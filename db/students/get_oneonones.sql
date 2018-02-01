7df49d2df9963db8449a95f6ca90504afd5e434f
Jonathan May <jmay4826@gmail.com>
2018-02-01 14:28:25 -0600

    update sql for dm_id

--- db/students/get_oneonones.sql
+++ db/students/get_oneonones.sql
@@ -1,10 +1,10 @@
-SELECT oneonones.*, students.first_name, students.last_name
+SELECT oneonones.*, students.first_name, students.last_name, students.dm_id
 FROM students
 	LEFT JOIN 
-		(SELECT max(id) as maxid, student_id 
+		(SELECT max(id) as maxid, dm_id 
 		FROM oneonones 
-		GROUP BY student_id) 
+		GROUP BY dm_id) 
 		AS x
-	ON student_id=dm_id
+	ON students.dm_id=x.dm_id
 	LEFT JOIN oneonones
 	ON x.maxid=oneonones.id
