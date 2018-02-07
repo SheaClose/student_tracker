a90bc085801434e756e980a59c7e1e47686cf72a
Jonathan May <jmay4826@gmail.com>
2018-02-06 20:26:15 -0600

    clean up console

--- src/components/Home/OneOnOneOutliers/OneOnOneOutliers.js
+++ src/components/Home/OneOnOneOutliers/OneOnOneOutliers.js
@@ -1,14 +1,6 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn
-} from 'material-ui/Table';
 import Card, { CardTitle, CardText } from 'material-ui/Card';
 import List from 'material-ui/List';
 
@@ -17,14 +9,14 @@ import OneOnOneDetail from '../../OneOnOnes/OneOnOneDetail';
 const OneOnOneOutliers = props => {
   const { outliers = { oneonones: [] } } = props;
   const { oneonones } = outliers;
-  console.log(oneonones);
+
   return (
     <Card style={props.style}>
       <CardTitle>One-on-Ones</CardTitle>
       <CardText>
         {oneonones.map(student =>
           student.oneonones.map(detail => (
-            <List>
+            <List key={detail.id}>
               <p>{detail.first_name}</p>
               <OneOnOneDetail detail={detail} />
             </List>
